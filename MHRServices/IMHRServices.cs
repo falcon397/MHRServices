048bdecc380dbf3e7600cbe2402f66194b7e827b
falcon397 <michaelhuckleberry.mh@gmail.com>
2017-10-17 20:36:19 -0700

    Everything works

---
+++ MHRServices/IMHRServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MHRServices
+{
+    [ServiceContract]
+    public interface IMHRService
+    {
+        //To Do: Make an actual object for each kind of json you might get
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "/save")]
+        string processJSON(List<Dictionary<string,object>> jsonString);
+    }
+}
+
---
+++ MHRServices/MHRService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MHRServices
+{
+    public class MHRService : IMHRService
+    {
+        public string processJSON(List<Dictionary<string, object>> jsonString)
+        {
+            return "success";
+        }
+
+    }
+}
